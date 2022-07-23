4caa7aba154d953db4ff6c0a3129d8c8ab847340
Collins Tatang <makungongcollins@gmail.com>
2022-07-23 09:11:13 +0100

    new

--- view.php
+++ view.php
@@ -30,42 +30,36 @@ $comment = $_POST['comment'];
     <link rel="stylesheet" href="styles.css" type="text/css">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
 </head>
 
 
+
 <body>
-   
 
-    <table>
+    <a href='reglist.php' class='btn btn-primary btn-sm'> &lt; Back </a>
+    <div class="table-form">
         <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-            <th>number</th>
-            <th>role</th>
-            <th>referer</th>
-            <th>mostlike</th>
-            <th>improve</th>
-            <th>comment</th>
-            <th>Actions</th>
-
-
-
-        <tr>
-          
+            <td><b>name</b></td>
             <td><?php echo $row['name']; ?></td>
+        </tr>
+        <br>
+        <tr>
+            <td><b>email</b></td>
             <td><?php echo $row['email']; ?></td>
+        </tr>
+        <br>
+        <tr>
+            <td><b>number</b></td>
             <td><?php echo $row['number']; ?></td>
-            <td><?php echo $row['role']; ?></td>
-            <td><?php echo $row['referer']; ?></td>
-            <td><?php echo $row['mostlike']; ?></td>
-            <td><?php echo $row['improve']; ?></td>
-            <td><?php echo $row['comment']; ?></td>
-</tr>
-<div>
-            <a href='reglist.php' class='btn btn-primary btn-sm'> &lt; Back </a>
-            </div>
-    </table>
+
+        </tr>
+    </div>
+
+
+
+
 </body>
+
 
 </html>
