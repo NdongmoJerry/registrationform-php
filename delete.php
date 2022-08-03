78992d86ea4a75bc3e93c285436abad84c18883b
Collins Tatang <makungongcollins@gmail.com>
2022-08-03 13:34:07 +0100

    delete alert created

--- delete.php
+++ delete.php
@@ -5,6 +5,9 @@ include('config.php');
 
 $sql = "DELETE FROM registration WHERE id='" . $_GET["id"] . "'";
 if (mysqli_query($conn, $sql)) {
+    echo "Record deletd successfully";
+}else{
+echo "record has not been deleted";
 }
  header('Location: reglist.php');
 ?>
